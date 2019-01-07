39fa8c232c08dd25ed5aadd3d824327be3917d58
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-01-07 17:08:07 -0500

    Created a Header Title component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+export const HeaderTitle = () => {
+  return (
+    <div className="title">
+      <h1>Lambda School</h1>
+      <h2>@LambdaSchool . 7 jan</h2>
+    </div>
+  );
+};
