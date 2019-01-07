01cc35d1ed495521485e36e4dc60ad85e687f912
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-01-07 16:56:17 -0500

    Created an image thumbnail component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+export const ImageThumbNail = () => {
+  return (
+    <img
+      className="logo"
+      src="https://ibin.co/3whrpKSBbZ81.png"
+      alt="Lambda Logo"
+    />
+  );
+};
